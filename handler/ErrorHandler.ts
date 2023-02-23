872721f367a020f68020887cb791e21bed1111be
devPhytols <devphytols@gmail.com>
2023-02-23 18:50:47 -0300

    Fix Handler Module

--- handler/ErrorHandler.ts
+++ handler/ErrorHandler.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-function errorHandler(err: Error, req: Request, res: Response) {
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
     console.error(err.stack);
     res.status(500).send('Ocorreu um erro na execução!');
 }
